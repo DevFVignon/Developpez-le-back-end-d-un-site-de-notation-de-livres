0e03a65f1e753c63ec0ab1ecb735f203c5e34a6a
FVignon <florentvignon.developpeur@gmail.com>
2024-06-12 10:02:49 +0200

    optimisation des images ok, appliquée aux créations et modifications de livres

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,11 +1,16 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const path = require ('path');
+const fs = require ('fs');
 
+//dictionnaire pour générer l'extension du fichier image
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jepg',
   'image/png': 'png'
 };
 
+//destination et nom du fichier
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
@@ -17,5 +22,28 @@ const storage = multer.diskStorage({
   }
 });
 
+//on exporte la gestion de fichier storage ou on s'occupe simplement des 
+//à chaque fois d'un fichier avec le champ de formulaire nommé 'image'.
 module.exports = multer({storage: storage}).single('image');
 
+module.exports.resizeImage = (req, res, next) => {
+  if (!req.file){return next}
+    const filePath = req.file.path;
+    const fileName = req.file.filename;
+    const outputFilePath = path.join('images', `resized_${fileName}`);
+
+    //sharp pour redimensionner l'image qui est traité au niveau du path indiqué
+    //puis on supprime le fichier qui était enr. avec storage pour le remplacer
+    sharp(filePath)
+    .resize({width: 404, heigh: 510})
+    .toFile(outputFilePath)
+    .then(()=>{
+      fs.unlink(filePath, ()=>{
+        req.file.path = outputFilePath;
+        next();
+      })
+    })
+    .catch((error)=>{console.log(error);
+      return next();
+    });
+};
--- routes/book.js
+++ routes/book.js
@@ -11,10 +11,10 @@ const bookCtrl = require('../controllers/book');
 router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/', auth, multer, multer.resizeImage, bookCtrl.createBook);
 router.post('/:id/rating', auth, bookCtrl.createRating);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
-router.delete('/:id', auth, multer, bookCtrl.deleteBook);
+router.put('/:id', auth, multer, multer.resizeImage, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 
 module.exports = router;
